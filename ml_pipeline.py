1c43635821f9d4228976ff6307118a699b53313f
AaronShetty2005 <shettyaaron07@gmail.com>
2025-12-09 18:29:40 +0530

    update

--- ml_pipeline.py
+++ ml_pipeline.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 print("=== ML CI Pipeline ===")
 
 
-X = np.array([1], [2], [3])
+X = np.array([[1], [2], [3]])
 y = np.array([2, 4, 6])
 
 
@@ -31,4 +31,3 @@ else:
     print(" VALIDATION FAILED")
     sys.exit(1)
 
-    
